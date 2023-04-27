4355052454894ada85aebe02500b1d05d8ee8daa
albert.barsegyan <albert.barsegyan@gohacktech.com>
2023-04-27 17:05:56 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Orderbook } from './components/Orderbook/Orderbook.jsx'
+
+function App () {
+
+  return (
+    <div>
+      <Orderbook/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Orderbook/Orderbook.jsx
@@ -0,0 +1,7 @@
+export function Orderbook () {
+  return (
+    <div>
+
+    </div>
+  )
+}
